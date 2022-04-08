24dbc4d88a0af150b4f15314f8d14c4ee178af9a
Hansiddh <88960895+Hansiddh@users.noreply.github.com>
2022-04-08 15:52:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var fruits=["apple","blue berry","strawberry"];
+console.log(fruits);
+document.getElementById("array_name").innerHTML=fruits;
+
+var firstfruit=fruits[0];
+console.log(firstfruit);
+document.getElementById("first_fruit").innerHTML=firstfruit;
+
+var thirdfruit=fruits[2];
+console.log(thirdfruit);
+document.getElementById("third_fruit").innerHTML=thirdfruit;
